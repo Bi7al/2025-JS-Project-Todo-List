eff07fe54dd8eeae943dcb993ea9a51b728edc66
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-01-01 10:20:24 +0500

    remaining : Deletion and Caching

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ const newTodoText = document.getElementById("todo-input");
 const todoList = document.getElementById("task-list");
 let myTodos = [];
 function addtask() {
-    const newTask = newTodoText.value.trim();
-    if (newTask.length > 0) {
+    const newTask = {
+        text: newTodoText.value.trim(),
+        completed: false
+    }
+    if (newTask.text.length > 0) {
+
         myTodos.push(newTask);
     }
     else {
@@ -14,7 +18,25 @@ function addtask() {
 
 }
 function renderTodos() {
+    todoList.innerHTML = '';
     myTodos.forEach((task, index) => {
-        todoList.innerText += task + index
+        const newItem = document.createElement("div");
+        newItem.className = "task-item"
+        newItem.innerHTML = `
+        <input type="checkbox" onChange=completeTask(${index}) >
+        <p id="task-${index}">${task.text}</p>`
+        todoList.append(newItem);
     })
+}
+function completeTask(taskId) {
+    myTodos[taskId].completed = !myTodos[taskId].completed;
+    const taskStatus = myTodos[taskId].completed;
+    if (taskStatus == true) {
+        document.getElementById(`task-${taskId}`).classList.add("completed")
+    }
+    else {
+        document.getElementById(`task-${taskId}`).classList.remove("completed")
+    }
+
+
 }
