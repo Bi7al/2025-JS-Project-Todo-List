91c3a1ea07d8e7e2db001eea434f933f98269dad
Muhammad Bilal <bilal.muhammad4573@gmail.com>
2025-01-03 19:46:32 +0500

    Added Weather API

--- script.js
+++ script.js
@@ -1,72 +1,108 @@
-const newTodoText = document.getElementById("todo-input");
-const todoList = document.getElementById("task-list");
+//  Weather api
+let weather_data;
+async function getWeather() {
+    const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=islamabad';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '0aa443960emsh81e18847fc2af04p1c86cdjsnd162e2ab6195',
+            'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
+        }
+    };
 
-let myTodos = [];
-loadFromStorage();
-renderTodos();
-function savetoStorage() {
-    localStorage.setItem("todos", JSON.stringify(myTodos));
-}
-function loadFromStorage() {
-    if (localStorage.getItem("todos")) {
-        myTodos = JSON.parse(localStorage.getItem("todos"));
-    }
-    else {
-        myTodos = [];
-    }
-}
-function addtask() {
-    const newTask = {
-        id: "",
-        text: newTodoText.value.trim(),
-        completed: false
-    }
-    if (newTask.text.length > 0) {
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        weather_data = {
+            "weather": result.current.condition.text,
+            "temprature": result.current.feelslike_c
+        }
 
-        myTodos.push(newTask);
+    } catch (error) {
+        console.error(error);
     }
-    else {
-        alert("NO text to Add")
-    }
-    newTodoText.value = '';
+
+}
+getWeather().then(main);
+
+//
+//
+// Main To do Logic
+
+
+function main() {
+    document.getElementById("weather").innerText = weather_data.weather;
+    document.getElementById("temp").innerText = weather_data.temprature + "° Celsius"
+    const newTodoText = document.getElementById("todo-input");
+    const todoList = document.getElementById("task-list");
+
+    let myTodos = [];
+    loadFromStorage();
     renderTodos();
-    savetoStorage();
+    function savetoStorage() {
+        localStorage.setItem("todos", JSON.stringify(myTodos));
+    }
+    function loadFromStorage() {
+        if (localStorage.getItem("todos")) {
+            myTodos = JSON.parse(localStorage.getItem("todos"));
+        }
+        else {
+            myTodos = [];
+        }
+    }
+    function addtask() {
+        const newTask = {
+            id: "",
+            text: newTodoText.value.trim(),
+            completed: false
+        }
+        if (newTask.text.length > 0) {
 
-}
-function renderTodos() {
-    todoList.innerHTML = '';
-    if (myTodos.length > 0) {
-        myTodos.forEach((task, index) => {
-            task.id = index;
-            const newItem = document.createElement("div");
-            newItem.className = "task-item"
-            newItem.innerHTML = `
+            myTodos.push(newTask);
+        }
+        else {
+            alert("NO text to Add")
+        }
+        newTodoText.value = '';
+        renderTodos();
+        savetoStorage();
+
+    }
+    function renderTodos() {
+        todoList.innerHTML = '';
+        if (myTodos.length > 0) {
+            myTodos.forEach((task, index) => {
+                task.id = index;
+                const newItem = document.createElement("div");
+                newItem.className = "task-item"
+                newItem.innerHTML = `
             <div class="task-title">
             <input type="checkbox" onChange=completeTask(${index}) >
             <button onclick=deleteTask(${index})><i class="fa-solid fa-trash"></i></button>
             </div>
             <p id="task-${index}">${task.text}</p>
         `
-            todoList.append(newItem);
-        })
+                todoList.append(newItem);
+            })
+        }
+        else {
+            todoList.innerHTML = '<p id="no-tasks">Add tasks to list</p>';
+        }
     }
-    else {
-        todoList.innerHTML = '<p id="no-tasks">Add tasks to list</p>';
+    function completeTask(taskId) {
+        myTodos[taskId].completed = !myTodos[taskId].completed;
+        const taskStatus = myTodos[taskId].completed;
+        if (taskStatus == true) {
+            document.getElementById(`task-${taskId}`).classList.add("completed")
+        }
+        else {
+            document.getElementById(`task-${taskId}`).classList.remove("completed")
+        }
     }
-}
-function completeTask(taskId) {
-    myTodos[taskId].completed = !myTodos[taskId].completed;
-    const taskStatus = myTodos[taskId].completed;
-    if (taskStatus == true) {
-        document.getElementById(`task-${taskId}`).classList.add("completed")
-    }
-    else {
-        document.getElementById(`task-${taskId}`).classList.remove("completed")
-    }
-}
-function deleteTask(taskId) {
+    function deleteTask(taskId) {
 
-    myTodos = myTodos.filter((task) => task.id != taskId);
-    savetoStorage();
-    renderTodos();
+        myTodos = myTodos.filter((task) => task.id != taskId);
+        savetoStorage();
+        renderTodos();
+    }
 }
